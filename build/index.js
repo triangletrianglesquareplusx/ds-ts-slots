4ddf6332556b6195dbdb9d29d7c796c2fefe9416
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-04-10 15:00:33 +0300

    structuring slot machine class

--- build/index.js
+++ build/index.js
@@ -1,10 +1,33 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const configuration_1 = __importDefault(require("./configuration"));
 class SlotMachine {
-    constructor(reelsCount, rowsCount) {
-        this.reelsCount = reelsCount;
-        this.rowsCount = rowsCount;
+    constructor() {
+        this.reelsCount = configuration_1.default.reelsCount;
+        this.rowsCount = configuration_1.default.rowsCount;
+        this.reels = configuration_1.default.reels;
+        this.result = [];
+    }
+    get reelsCountValue() {
+        return this.reelsCount;
+    }
+    get rowsCountValue() {
+        return this.reelsCount;
+    }
+    get resultValue() {
+        return this.result;
     }
     static generateRandomIndexForReel(reelLength) {
         return Math.floor(Math.random() * reelLength) + 1;
     }
+    spin() {
+    }
+    evaluateResult() {
+        return 1;
+    }
 }
+const mySlotMachine = new SlotMachine();
+console.log(mySlotMachine.reelsCountValue);
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,42 @@
+import configuration from './configuration'
+
 class SlotMachine {
   protected reelsCount: number;
   protected rowsCount: number;
+  protected reels: number[][];
+  private result: number[][];
 
-  constructor(reelsCount: number, rowsCount: number) {
-    this.reelsCount = reelsCount;
-    this.rowsCount = rowsCount;
+  constructor() {
+    this.reelsCount = configuration.reelsCount;
+    this.rowsCount = configuration.rowsCount;
+    this.reels = configuration.reels;
+    this.result = [];
+  }
+  
+  get reelsCountValue(){
+    return this.reelsCount;
+  }
+
+  get rowsCountValue(){
+    return this.reelsCount;
+  }
+
+  get resultValue(){
+    return this.result;
   }
 
   static generateRandomIndexForReel(reelLength: number) {
     return Math.floor(Math.random() * reelLength) + 1;
   }
+
+  spin(){
+
+  }
+
+  evaluateResult(): number{
+    return 1;
+  }
 }
+
+const mySlotMachine = new SlotMachine();
+console.log(mySlotMachine.reelsCountValue);
