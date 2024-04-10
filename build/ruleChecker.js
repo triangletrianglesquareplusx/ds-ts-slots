43b1882bcd469038a104ce870eb08eee07a6739d
triangletrianglesquareplusx <preslavgetov@gmail.com>
2024-04-10 18:27:46 +0300

    primitive winning condition check - needs refactor

--- build/ruleChecker.js
+++ build/ruleChecker.js
@@ -6,11 +6,28 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.RuleChecker = void 0;
 const configuration_1 = __importDefault(require("./configuration"));
 class RuleChecker {
+    constructor() {
+        this.pattern = [0, 1, 2];
+    }
     static checkForWinningConditions(slotMatrix) {
-        console.log(RuleChecker.lines);
+        let result = "";
+        //rule 1
+        const lineToSum = slotMatrix[0].reduce((prev, acc) => acc + prev, 0);
+        lineToSum % slotMatrix[0].length == 0
+            ? (result = "you win")
+            : (result = "you lose");
+        //rule 2
+        const lineToSum2 = slotMatrix[1].reduce((prev, acc) => acc + prev, 0);
+        lineToSum % slotMatrix[0].length == 0
+            ? (result = "you win")
+            : (result = "you lose");
+        //rule 3
+        // const lineToSum3 = slotMatrix[2].reduce((prev, acc) => acc + prev, 0);
+        // lineToSum % slotMatrix[0].length == 0
+        //   ? (result = "you win")
+        //   : (result = "you lose");
     }
-    static calculatePayOut() {
-    }
+    static calculatePayOut() { }
 }
 exports.RuleChecker = RuleChecker;
 RuleChecker.lines = configuration_1.default.lines;
--- src/ruleChecker.ts
+++ src/ruleChecker.ts
@@ -1,13 +1,32 @@
 import configuration from "./configuration";
 
 export class RuleChecker {
-    private static lines: number[][] = configuration.lines;
+  private static lines: number[][] = configuration.lines;
+  private pattern: number[] = [0,1,2];
 
-    static checkForWinningConditions(slotMatrix: number[][]):void{
-        console.log(RuleChecker.lines)
-    }
+  static checkForWinningConditions(slotMatrix: number[][]): void {
+    let result = "";
 
-    static calculatePayOut(){
+    //rule 1
+    const lineToSum = slotMatrix[0].reduce((prev, acc) => acc + prev, 0);
+    lineToSum % slotMatrix[0].length == 0
+      ? (result = "you win")
+      : (result = "you lose");
 
-    }
+    //rule 2
+    const lineToSum2 = slotMatrix[1].reduce((prev, acc) => acc + prev, 0);
+
+    lineToSum % slotMatrix[0].length == 0
+      ? (result = "you win")
+      : (result = "you lose");
+
+    //rule 3
+    // const lineToSum3 = slotMatrix[2].reduce((prev, acc) => acc + prev, 0);
+
+    // lineToSum % slotMatrix[0].length == 0
+    //   ? (result = "you win")
+    //   : (result = "you lose");
+  }
+
+  static calculatePayOut() {}
 }
